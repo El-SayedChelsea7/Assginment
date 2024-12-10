f8585e38c9e869aa4b50bcd8a9df9b8cbad5717f
hp <hp@DEVELSAYED>
2024-12-10 15:00:31 +0200

    Add project files.

---
+++ Assginment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assginment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
